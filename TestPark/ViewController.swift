7ec36b0fc3875820b2cf8fc0e7cab49133d99e23
oliviero rossi <oliviero.rossi@iol.it>
2017-04-26 14:53:34 +0200

    Initial Commit

---
+++ TestPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPark
+//
+//  Created by oliviero rossi on 26/04/17.
+//  Copyright © 2017 gianluigi nero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
